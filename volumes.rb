b2dd02432ac4da441cdc2120a598acaf29f45b39
cc2RG <rbgalloway88@gmail.com>
2015-11-17 17:00:52 +0000

    Volume functions first draft

--- volumes.rb
+++ volumes.rb
@@ -5,25 +5,29 @@ def cube_vol(l, w, h)
  end  
 
 def sphere_vol(r)
-  pi = 3.14159265358979
-  (4/3)*Math::PI*(r**3)
+  (4/3) * Math::PI * (r**3)
+   return
 end 
 
 def cylinder_vol(r,h)
-Math::PI 
+  Math::PI * r**2 * h
+  return
 end  
 
 def rec_prisim_vol(w,h,l)
-
-
+  w * h * l
+  return
 end  
 
 def cone_vol(r,h)
-
+  Math::PI * (r**2) * (h/3) 
+  return
 end
 
 def capsule(r,a)
-
+  Math::PI**2 * ((4/3)*r + a)
+  return
 end  
+
 
 puts sphere_vol(3)
