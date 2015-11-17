ae91fc02a4fa9ffa17dba10741fb69498d3cdb71
cc2RG <rbgalloway88@gmail.com>
2015-11-17 16:36:07 +0000

    volumes first draft

---
+++ volumes.rb
@@ -0,0 +1,29 @@
+pi = 3.14159265358979
+
+def cube_vol(l, w, h)
+   l * w * h
+ end  
+
+def sphere_vol(r)
+  pi = 3.14159265358979
+  (4/3)*Math::PI*(r**3)
+end 
+
+def cylinder_vol(r,h)
+Math::PI 
+end  
+
+def rec_prisim_vol(w,h,l)
+
+
+end  
+
+def cone_vol(r,h)
+
+end
+
+def capsule(r,a)
+
+end  
+
+puts sphere_vol(3)
